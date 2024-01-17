c795047fb6053df9b22c68fe0f694818e41b2145
Frank Hu <tianyaoh@hotmail.com>
2024-01-17 23:30:06 +0000

    added map_file_arg and rviz_node in map_server.launch.py

--- map_server/launch/map_server.launch.py
+++ map_server/launch/map_server.launch.py
@@ -1,13 +1,27 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    
-    map_file = os.path.join(get_package_share_directory('map_server'), 'config', 'warehouse_map_sim.yaml')
+
+    map_file_arg = DeclareLaunchArgument(
+        "map_file", default_value="warehouse_map_sim.yaml"
+    )
+
+    map_file = os.path.join(get_package_share_directory('map_server'), 'config', map_file_arg)
+    rviz_config_dir = os.path.join(get_package_share_directory('map_server'), 'config', 'launch_part.rviz')
 
     return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir]),
+
         Node(
             package='nav2_map_server',
             executable='map_server',
@@ -24,5 +38,6 @@ def generate_launch_description():
             output='screen',
             parameters=[{'use_sim_time': True},
                         {'autostart': True},
-                        {'node_names': ['map_server']}])            
+                        {'node_names': ['map_server']}]),
+
         ])
