ee98be67975f686205440e233252fa46509a5d09
Frank Hu <tianyaoh@hotmail.com>
2024-01-18 16:01:01 +0000

    added use_sim_time arg with default False

--- map_server/launch/map_server.launch.py
+++ map_server/launch/map_server.launch.py
@@ -15,8 +15,14 @@ def generate_launch_description():
     #everything default as sim
     return LaunchDescription([
         DeclareLaunchArgument(
+            'use_sim_time',
+            default_value="False", 
+            description='Turn on/off sim time setting'
+        ),
+
+        DeclareLaunchArgument(
             'map_file',
-            default_value="warehouse_map_sim.yaml", 
+            default_value="warehouse_map_real.yaml", 
             description='name of map_file within map_server/config'
         ),
 
@@ -25,7 +31,7 @@ def generate_launch_description():
             executable='rviz2',
             output='screen',
             name='rviz_node',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': LaunchConfiguration("use_sim_time")}],
             arguments=['-d', rviz_config_dir]),
 
         Node(
@@ -33,7 +39,7 @@ def generate_launch_description():
             executable='map_server',
             name='map_server',
             output='screen',
-            parameters=[{'use_sim_time': True}, 
+            parameters=[{'use_sim_time': LaunchConfiguration("use_sim_time")}, 
                         {'yaml_filename':PathJoinSubstitution([FindPackageShare("map_server"), 'config', LaunchConfiguration("map_file")])}
                         ]),
 
@@ -42,7 +48,7 @@ def generate_launch_description():
             executable='lifecycle_manager',
             name='lifecycle_manager_mapper',
             output='screen',
-            parameters=[{'use_sim_time': True},
+            parameters=[{'use_sim_time': LaunchConfiguration("use_sim_time")},
                         {'autostart': True},
                         {'node_names': ['map_server']}]),
 
