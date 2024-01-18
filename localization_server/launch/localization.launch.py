bc085c80237006988457a2b9acc06f1bd60f3461
Frank Hu <tianyaoh@hotmail.com>
2024-01-18 16:11:57 +0000

    localization adapt to real robot

--- localization_server/launch/localization.launch.py
+++ localization_server/launch/localization.launch.py
@@ -14,8 +14,14 @@ def generate_launch_description():
     
     return LaunchDescription([
         DeclareLaunchArgument(
+            'use_sim_time',
+            default_value="False", 
+            description='Turn on/off sim time setting'
+        ),
+
+        DeclareLaunchArgument(
             'map_file',
-            default_value="warehouse_map_sim.yaml", 
+            default_value="warehouse_map_real.yaml", 
             description='name of map_file within map_server/config'
         ),
 
@@ -24,7 +30,7 @@ def generate_launch_description():
             executable='map_server',
             name='map_server',
             output='screen',
-            parameters=[{'use_sim_time': True}, 
+            parameters=[{'use_sim_time': LaunchConfiguration("use_sim_time")}, 
                         {'yaml_filename':PathJoinSubstitution([FindPackageShare("map_server"), 'config', LaunchConfiguration("map_file")])}]
         ),
             
@@ -41,7 +47,7 @@ def generate_launch_description():
             executable='lifecycle_manager',
             name='lifecycle_manager_localization',
             output='screen',
-            parameters=[{'use_sim_time': True},
+            parameters=[{'use_sim_time': LaunchConfiguration("use_sim_time")},
                         {'autostart': True},
                         {'node_names': ['map_server', 'amcl']}]
         ),
