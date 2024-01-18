ba6c7193fe6508f8bfb49f266507cd0edc4b4f44
Frank Hu <tianyaoh@hotmail.com>
2024-01-18 06:01:12 +0000

    path_planner working in SIM

---
+++ path_planner_server/launch/pathplanner.launch.py
@@ -0,0 +1,73 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    controller_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'controller.yaml')
+    bt_navigator_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'bt.yaml')
+    planner_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'planner_server.yaml')
+    recovery_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'recovery.yaml')
+
+    rviz_config_dir = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'pathplanning.rviz')
+    
+    return LaunchDescription([    
+        # Node(
+        #     package='rviz2',
+        #     executable='rviz2',
+        #     output='screen',
+        #     name='rviz_node',
+        #     parameters=[{'use_sim_time': True}],
+        #     arguments=['-d', rviz_config_dir]),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher_base_link_2_robot_base_link',
+            output='screen',
+            emulate_tty=True,
+            arguments=['0', '0', '0', '0', '0', '0', 'robot_base_link', 'base_link']
+        ),
+
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            name='controller_server',
+            output='screen',
+            parameters=[controller_yaml],
+            remappings=[('/cmd_vel', '/robot/cmd_vel')]),
+
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=[planner_yaml]),
+            
+        Node(
+            package='nav2_recoveries',
+            executable='recoveries_server',
+            name='recoveries_server',
+            parameters=[recovery_yaml],
+            output='screen'),
+
+        Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            parameters=[bt_navigator_yaml],
+            remappings=[('/cmd_vel', '/robot/cmd_vel')]),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_pathplanner',
+            output='screen',
+            parameters=[{'autostart': True},
+                        {'node_names': ['planner_server',
+                                        'controller_server',
+                                        'recoveries_server',
+                                        'bt_navigator']}])
+    ])
