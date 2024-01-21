d464286f9b3f26710ae3a0250779feed61abcb87
Frank Hu <tianyaoh@hotmail.com>
2024-01-21 05:38:19 +0000

    goto pre_load and goto final pose finished

---
+++ nav2_apps/scripts/move_shelf_to_ship.py
@@ -0,0 +1,113 @@
+import time
+from geometry_msgs.msg import PoseStamped
+from rclpy.duration import Duration
+import rclpy
+
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+
+# Shelf positions for picking
+shelf_positions = {
+    "shelf_start": [5.273, -2.853],
+    "loading_pose": [5.0, 0.0]
+}
+
+shipping_destinations = {
+    "shelf_end" : [0.5, -3.0]
+}
+
+'''
+Basic item picking demo. In this demonstration, the expectation
+is that a person is waiting at the item shelf to put the item on the robot
+and at the pallet jack to remove it
+(probably with a button for 'got item, robot go do next task').
+'''
+
+SIM_START_POS_X = -0.159
+SIM_START_POS_Y = 0.0
+SIM_START_ORN_Z = 0.0
+
+def get_sim_initial_pose(nav):
+    # Set initial pose
+    initial_pose = PoseStamped()
+    initial_pose.header.frame_id = 'map'
+    initial_pose.header.stamp = nav.get_clock().now().to_msg()
+    initial_pose.pose.position.x = SIM_START_POS_X
+    initial_pose.pose.position.y = SIM_START_POS_Y
+    initial_pose.pose.orientation.z = SIM_START_ORN_Z
+    return initial_pose
+
+def main():
+    # Recieved virtual request for picking item at Shelf A and bringing to
+    # worker at the pallet jack 7 for shipping. This request would
+    # contain the shelf ID ("shelf_A") and shipping destination ("pallet_jack7")
+    ####################
+    request_item_location = 'loading_pose'
+    request_destination = 'shelf_end'
+    ####################
+
+    rclpy.init()
+
+    navigator = BasicNavigator()
+
+    # Set robot initial pose
+    initial_pose = get_sim_initial_pose(navigator)
+    #announcing initial pose set
+    print("Simulation initial pose set")
+
+    # Wait for navigation to activate fully
+    navigator.waitUntilNav2Active()
+
+    shelf_item_pose = PoseStamped()
+    shelf_item_pose.header.frame_id = 'map'
+    shelf_item_pose.header.stamp = navigator.get_clock().now().to_msg()
+    shelf_item_pose.pose.position.x = shelf_positions[request_item_location][0]
+    shelf_item_pose.pose.position.y = shelf_positions[request_item_location][1]
+    shelf_item_pose.pose.orientation.z = 1.0
+    shelf_item_pose.pose.orientation.w = 0.0
+    print('Received request for item picking at ' + request_item_location + '.')
+    navigator.goToPose(shelf_item_pose)
+
+    # Do something during your route
+    # (e.x. queue up future tasks or detect person for fine-tuned positioning)
+    # Print information for workers on the robot's ETA for the demonstration
+    i = 0
+    while not navigator.isTaskComplete():
+        i = i + 1
+        feedback = navigator.getFeedback()
+        if feedback and i % 5 == 0:
+            print('Estimated time of arrival at ' + request_item_location +
+                  ' for worker: ' + '{0:.0f}'.format(
+                      Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
+                  + ' seconds.')
+
+    result = navigator.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print('Got product from ' + request_item_location +
+              '! Bringing product to shipping destination (' + request_destination + ')...')
+        shipping_destination = PoseStamped()
+        shipping_destination.header.frame_id = 'map'
+        shipping_destination.header.stamp = navigator.get_clock().now().to_msg()
+        shipping_destination.pose.position.x = shipping_destinations[request_destination][0]
+        shipping_destination.pose.position.y = shipping_destinations[request_destination][1]
+        shipping_destination.pose.orientation.z = 1.0
+        shipping_destination.pose.orientation.w = 0.0
+        navigator.goToPose(shipping_destination)
+
+    elif result == TaskResult.CANCELED:
+        print('Task at ' + request_item_location +
+              ' was canceled. Returning to staging point...')
+        initial_pose.header.stamp = navigator.get_clock().now().to_msg()
+        navigator.goToPose(initial_pose)
+
+    elif result == TaskResult.FAILED:
+        print('Task at ' + request_item_location + ' failed!')
+        exit(-1)
+
+    while not navigator.isTaskComplete():
+        pass
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
